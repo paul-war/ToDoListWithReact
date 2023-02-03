2ce9768b3cf94f7fcb13c67e321c370eb96bacfa
paul-war <pablodanielguerra@gmail.com>
2023-02-03 14:24:21 +0000

    version2

--- src/js/component/HandleInput.jsx
+++ src/js/component/HandleInput.jsx
@@ -4,7 +4,11 @@ export function HandleInput (props) {
 
     const [counter, setCounter] = useState(0);
     const [list, setList] = useState([]);
+    const [inputValue, setInputValue] = useState("");
 
+    const handleFocus = (event) => {
+      setInputValue("");
+    };
 
     const handleAdd = (event) => {
         if (event.key === "Enter") {
@@ -14,21 +18,26 @@ export function HandleInput (props) {
         } 
       };
 
-    const handleX = () => {
-
-    }
+      const handleX = (event) => {
+        const index = list.indexOf(event.target.previousSibling.data);
+        if (index !== -1) {
+          setList([...list.slice(0, index), ...list.slice(index + 1)]);
+          setCounter(counter - 1);
+        }
+    };
     
 
 	return (
 		<div className="text-center container">
 			<h1>todos</h1>
 			<ul>
- 				<li><input onKeyDown={handleAdd} type={list}/></li>
+ 				<li><input onFocus={handleFocus} onKeyDown={handleAdd} type={props.list}/></li>
                 {list.map((item, index) => (
-                    <li key={index}>{item}<span>X</span></li>
+                    <li key={index}>{item}<span onClick={handleX}>X</span></li>
                 ))}
 				<li id="fixedcounter"><small>{(counter===0) ? "No tasks, add a task" : counter + " items left"}</small></li>
 			</ul>
 		</div>
 	);
 }
+
